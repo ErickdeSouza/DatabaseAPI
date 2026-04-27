9023a3218bfb161a5e7efbe62210452666e132c7
Lolezylol <erickjuliaodesouzajulao15@gmail.com>
2026-04-27 20:43:49 -0300

    some optimizations for API

--- app.py
+++ app.py
@@ -1,64 +1,62 @@
 from modules.T001 import FetchData
 from modules.T002 import getSecrets
-from modules.T003 import genToken
-from fastapi import FastAPI, Request, Depends
+from modules.T003 import Login
+from fastapi***REMOVED***  import FastAPI, Request, Depends
+
 
 envs = getSecrets().envs
 dbdata = FetchData(envs)
-auth = genToken(envs)
+login = Login
+login._ENVS = envs
 
-app = FastAPI(title="Database API", version="1.0.0")
+app = FastAPI(title="Database API", version="1.5.0")
+
+
+@app.get("/ping")
+def ping():
+***REMOVED***return {"ok": True, "result": "Pong!"}
 
 @app.post("/containers/login")
 async def clogin(request: Request):
-***REMOVED***body = await request.json()
-***REMOVED***user =  body.get("user")
-***REMOVED***key = body.get("password")
-***REMOVED***token = auth.generate_token(key, user)
-***REMOVED***if token:
-***REMOVED***return {"ok": True, "token": token}
+***REMOVED***return login(request)
 
-***REMOVED***return {"ok": False, "error": "invalid user/password"}
-
-@app.get("/containers/get", dependencies=[Depends(auth.auth_dependency)])
+@app.get("/containers/get", dependencies=[Depends(login.auth_dependency)])
 def ccreate(git: str = None, arg: bool = True):
 ***REMOVED***return dbdata.get(git, arg)
 
-@app.post("/containers/delete", dependencies=[Depends(auth.auth_dependency)])
+@app.post("/containers/delete", dependencies=[Depends(login.auth_dependency)])
 async def cdelete(request: Request):
 ***REMOVED***body = await request.json()
 ***REMOVED***return dbdata.delete(body["git"])
 
-@app.post("/containers/post", dependencies=[Depends(auth.auth_dependency)])
+@app.post("/containers/post", dependencies=[Depends(login.auth_dependency)])
 async def cpost(request: Request):
 ***REMOVED***body = await request.json()
 ***REMOVED***return dbdata.post(body)
 
-@app.get("/containers/code", dependencies=[Depends(auth.auth_dependency)])
+@app.get("/containers/code", dependencies=[Depends(login.auth_dependency)])
 async def fcode():
 ***REMOVED***return dbdata.fcode()
 
-@app.post("/containers/code", dependencies=[Depends(auth.auth_dependency)])
+@app.post("/containers/code", dependencies=[Depends(login.auth_dependency)])
 async def pcode(request: Request):
 ***REMOVED***body = await request.json()
 ***REMOVED***return dbdata.pcode(body)
 
-
-@app.get("/containers/gen", dependencies=[Depends(auth.auth_dependency)])
+@app.get("/containers/gen", dependencies=[Depends(login.auth_dependency)])
 async def fgen():
 ***REMOVED***return dbdata.fgen()
 
-@app.post("/containers/gen", dependencies=[Depends(auth.auth_dependency)])
+@app.post("/containers/gen", dependencies=[Depends(login.auth_dependency)])
 async def pgen(request: Request):
 ***REMOVED***body = await request.json()
 ***REMOVED***return dbdata.pgen(body)
 
-
-@app.post("/containers/update", dependencies=[Depends(auth.auth_dependency)])
+@app.post("/containers/update", dependencies=[Depends(login.auth_dependency)])
 async def update(request: Request):
 ***REMOVED***body = await request.json()
 ***REMOVED***return dbdata.upcontainer(body["git"])
 
-@app.get("/containers/commit", dependencies=[Depends(auth.auth_dependency)])
+@app.get("/containers/commit", dependencies=[Depends(login.auth_dependency)])
 def newupdate():
 ***REMOVED***return dbdata.getValue()
--- modules/T003.py
+++ modules/T003.py
@@ -1,11 +1,12 @@
-import hmac, time, hashlib
-from fastapi import Header, HTTPException
+from fastapi import Header, HTTPException, Request
+import hmac, time, hashlib, asyncio
 
 
 class genToken:
-***REMOVED***def __init__(self, env: dict):
-***REMOVED***self.secret_key = env["SECRET_KEY"]
-***REMOVED***self.user = env["USER"]
+***REMOVED***def __init__(self, env: dict|None):
+***REMOVED***if not env:
+***REMOVED******REMOVED***self.secret_key = env["SECRET_KEY"]
+***REMOVED******REMOVED***self.user = env["USER"]
 
 ***REMOVED***def generate_token(self, key, user, ttl_seconds: int = 3600):
 ***REMOVED***if (key, user) != (self.secret_key, self.user):
@@ -38,3 +39,25 @@ class genToken:
 
 ***REMOVED***if not self.validate_token(token):
 ***REMOVED******REMOVED***raise HTTPException(status_code=403, detail="Token inválido")
+***REMOVED***
+
+class Login(genToken):
+***REMOVED***_ENVS = None
+***REMOVED***
+***REMOVED***def __int__(self, request: Request|None = None):
+***REMOVED***super().__init__(Login._ENVS)
+***REMOVED***if request:
+***REMOVED******REMOVED***return self.signup(request)
+***REMOVED***
+***REMOVED***async def _signup(self, request: Request):
+***REMOVED***body = await request.json()
+***REMOVED***user =  str(body.get("user"))
+***REMOVED***key = str(body.get("password"))
+***REMOVED***token = self.generate_token(key, user)
+***REMOVED***if token:
+***REMOVED******REMOVED***return {"ok": True, "token": token}
+
+***REMOVED***return {"ok": False, "error": "invalid user/password"}
+***REMOVED***
+***REMOVED***def signup(self, req):
+***REMOVED***return asyncio.run(self._signup(req))
